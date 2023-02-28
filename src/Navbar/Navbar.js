0c0413b0682af73447a66b44e024801b30bd0de1
rmulrennan01 <rpmulrennan@gmail.com>
2023-02-28 15:56:42 -0600

    continued work on searchbar query

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './Navbar.css';
 import Search from '../TMDB/Search'; 
 import * as M from '@mui/material/';
@@ -12,7 +12,7 @@ function Navbar() {
     const [open, setOpen] = useState(false);
     const [input, setInput] = useState('');
     const [loading, setLoading] = useState(false);
-
+    const prevController = useRef();  
 
       function sleep(delay = 0) {
         return new Promise((resolve) => {
@@ -20,47 +20,13 @@ function Navbar() {
         });
       }
     
-      /*
-    useEffect(() => {
-        Search('star wars')
-        .then((result) =>{
-            console.log(result);
-        })
-        .catch((err) =>{
-            console.log(err)
-        })
-
-    }, [])
-
-    */
 
     useEffect(() => {
-        console.log('hey')
         let active = true;
 
         if (!loading) {
           return undefined;
         }
-    
-        /*
-        (async () => {
-          if (active && input != '') {
-            Search(input)
-            .then((result) =>{
-                console.log(result);
-                setOptions(result)
-            })
-            .catch((err) =>{
-                console.log(err)
-            })
-          }
-        })();
-        */
-      
-
-      
-
-    
         return () => {
           active = false;
         };
@@ -73,11 +39,19 @@ function Navbar() {
         }
     }, [open]);
 
+
+    //FUNCTION TO CALL UPON USER INPUT IN SEARCH BAR. THIS QUERIES RESULTS ON USER INPUT.
     const getData = (event, value, reason) => {
+        if (prevController.current) {
+            prevController.current.abort();
+        }
+        const controller = new AbortController();
+        const signal = controller.signal; 
+        prevController.current = controller; 
         
         if(value != ''){
             console.log(value);
-            Search(value)
+            Search(value, signal)
             .then((result) =>{
                 console.log(result);
                 setOptions(result.results)
@@ -85,8 +59,17 @@ function Navbar() {
             .catch((err) =>{
                 console.log(err)
             })
-            
         }
+    }
+
+    function ListboxItem (props) {
+        console.log(props);
+        return(
+            <>
+                <ul {...props} />
+                
+            </>
+        );
 
     }
 
@@ -95,7 +78,7 @@ function Navbar() {
         <div className = "navbar">
             <Autocomplete
                 id="asynch-search"
-                sx={{ width: 300 }}
+                sx={{ width: 300, backgroundColor: 'white' }}
                 open={open}
                 onOpen={() => {
                     setOpen(true);
@@ -104,14 +87,19 @@ function Navbar() {
                     setOpen(false);
                 }}
                 filterOptions={(x) => x}
-                getOptionLabel={(option) => option.title}
+                getOptionLabel={(option) => option.title + " " + option.id}
                 options={options}
                 loading={loading}
+                loadingText={"Loading..."}
+                noOptionsText="No Matches"
                 onInputChange ={getData}
+                ListboxComponent={ListboxItem}
+                
                 renderInput={(params) => (
                     <TextField
                     {...params}
-                    label="Asynchronous"
+                    label="Search film, tv, and cast"
+                 
                     InputProps={{
                         ...params.InputProps,
                         endAdornment: (
@@ -122,7 +110,7 @@ function Navbar() {
                         ),
                     }}
                     />
-                )}
+                    )}
                 />        
     </div>
     )
--- src/TMDB/Search.js
+++ src/TMDB/Search.js
@@ -7,17 +7,18 @@
 //EXAMPLE: https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
 
 
-const Search = async (query) => {
+const Search = async (query, signal) => {
 
     //TODO SPLIT BY SPACES
-    let films = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`)
+    let films = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`, {signal})
     
     //let data = await fetch("https://api.themoviedb.org/3/search/movie?api_key=" + process.env.REACT_APP_TMDB_API_KEY + "&" + query)
     let results_1 = await films.json();
 
+    /*
     let tv = await fetch(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&query=${query}&language=en-US&page=1&include_adult=false`)
     let results_2 = await tv.json();
-
+    */
 
     return results_1
 
