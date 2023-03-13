0bd45cb10ff083fc570933810e5c67df66d5870d
rmulrennan01 <rpmulrennan@gmail.com>
2023-03-13 07:44:11 -0500

    carousel progress

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import { motion, useScroll, useTransform, useSpring } from "framer-motion";
 import * as M from '@mui/material/';
 import * as Icons from '@mui/icons-material/';
@@ -8,48 +8,52 @@ import './Carousel.css';
 
 function Carousel(props) {
     const [position, setPosition] = useState([0])
-    const [spacing, setSpacing] = useState(props.spacing)
+    const [spacing, setSpacing] = useState(Number(12)); 
+    const [children, setChildren] = useState([]); 
+    const [loaded, setLoaded] = useState(true); 
+    const childRefs = useRef([]); 
 
     const { scrollYProgress } = useScroll() ;
     const y = useTransform(scrollYProgress, [0,1], [0,600]);
     const x = useTransform(scrollYProgress, [0,1], [0,600]);
 
     useEffect(() =>{
-
+        setChildren([...props.children])
+        setLoaded(true);
 
 
     }, []);
 
 
-    const buildPositions = () =>{
+    const buildChildren = (item, index) =>{
+
+        return(
+            <motion.div ref={(item)=>childRefs.current.push(item)} key={index+'carousel_child'}>
+                {item}
+            </motion.div>
+
+        )
 
 
     }
     
 
-    const buildItems = (item,index) => {
-
-        return(
-            <div className='carousel__child'>
-                {item.name}
-            </div>
-        )
-    }
 
 
 
   return (
     <div>
         <button className='carousel__btn__left' onClick={()=>setPosition(position-400)}> 
-            <Icons.ArrowBackIos  sx={{color:'red', scale:'250%', position}}/> 
+            <Icons.ArrowBackIos  sx={{color:'white', scale:'250%', position}}/> 
         </button>
 
         <button className='carousel__btn__right' onClick={()=>setPosition(position+400)}> 
-            <Icons.ArrowForwardIos  sx={{color:'red', scale:'250%'}}/> 
+            <Icons.ArrowForwardIos  sx={{color:'white', scale:'250%'}}/> 
         </button>
         <motion.div className='carousel__row' animate={{x:position}} > 
     
-            {props.children}
+            {loaded ? children.map(buildChildren) : null}
+            
 
         </motion.div>
 
